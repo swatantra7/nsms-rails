dab750e1428934797cb4026543fc56a31ae8ea19
swatantra7 <swatantrajha7@gmail.com>
2018-02-21 00:47:54 +0530

    typo correction

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,8 +5,5 @@ class Course < ActiveRecord::Base
   #@param [Hash]
   #@option opts [String] :subject The subject
   #@see http://example.com Description of URL
-  def hello(Hash)
-    code writting here
-  end
 
 end
