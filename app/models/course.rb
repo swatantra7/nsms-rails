770967ccb02c5235c22fd17e2d2beb2e22a78f34
swatantra7 <swatantrajha7@gmail.com>
2018-02-21 00:16:58 +0530

    THis commit refs  - changes in code as per requirement of nsms

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ActiveRecord::Base
+
+  #@example Reverse a string
+  #   "mystring.reverse" #=> "gnirtsym"
+  #@param [Hash]
+  #@option opts [String] :subject The subject
+  #@see http://example.com Description of URL
+  def hello(Hash)
+    code writting here
+  end
+
 end
